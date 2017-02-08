a7f4aa5978d4ab1e38a3a4fec3bbb6687ba3d2fd
newbienewbie <webbot@sina.com>
2017-02-08 21:28:06 +0800

    添加微信配置的密钥字段，更改数据库列名

--- lib/domain/wechat.sql
+++ lib/domain/wechat.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `wechat` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `appid` varchar(255) DEFAULT NULL,
+  `app_id` varchar(255) DEFAULT NULL,
   `token` varchar(255) DEFAULT NULL,
-  `encodingAESKey` varchar(255) DEFAULT NULL,
+  `encoding_aes_key` varchar(255) DEFAULT NULL,
+  `app_secret` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
