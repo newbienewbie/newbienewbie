225ad29fb533f356f544e68315c1ae0440a4bfde
newbienewbie <itminus@hotmail.com>
2018-12-29 20:49:58 +0800

    code : update EmailSender and make it run for Tecent Cloud

--- app/Services/EmailSender/MailSenderOptions.cs
+++ app/Services/EmailSender/MailSenderOptions.cs
@@ -5,5 +5,7 @@ namespace App.Services.EmailSender
         public string Host {get;set;} = "smtp.qq.com";
         public string User { get; set; }
         public string Key { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
     }
 }
