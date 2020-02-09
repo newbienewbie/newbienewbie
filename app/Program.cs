49c9b57899ed026bed0fafdf16280aed0d6bd64f
newbienewbie <webbot@sina.com>
2020-02-09 15:27:52 +0800

    Use Sqlite By Default

--- app/Program.cs
+++ app/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Areas.Identity.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace App
@@ -14,7 +16,13 @@ namespace App
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host =CreateWebHostBuilder(args).Build();
+            var sp = host.Services;
+            using(var scope = sp.CreateScope()){
+                var dbContext=scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
