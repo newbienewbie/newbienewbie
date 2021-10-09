35fdb3c03825bbd158f60764dd5e01b2b8724e50
xu lou <itminus@hotmail.com>
2021-10-10 01:42:39 +0800

    fix next/prev page link of index

--- generators/index.fsx
+++ generators/index.fsx
@@ -30,11 +30,11 @@ let generate' (ctx : SiteContents) (_: string) =
   let layoutForPostSet i psts =
     let nextPage =
       if i = (pages - 1) then "#"
-      else "/" + getFilenameForIndex (i + 1)
+      else "" + getFilenameForIndex (i + 1)
 
     let previousPage =
       if i = 0 then "#"
-      else "/" + getFilenameForIndex (i - 1)
+      else "" + getFilenameForIndex (i - 1)
 
     Layout.layout ctx "Home" [
       section [Class "hero is-info is-medium is-bold"] [
